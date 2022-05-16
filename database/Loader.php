2dc5eef675981860d50ef3a28ed9c6105bb21bf6
johnwaithira <waithirajon@gmail.com>
2022-05-16 13:51:28 +0300

    Kicks Nairobi

--- database/Loader.php
+++ database/Loader.php
@@ -2,10 +2,16 @@
     
     namespace Waithira\Kicksfootware\database;
     
+    use Dotenv\Dotenv;
+
     class Loader
     {
         public function env()
         {
+            require_once  dirname(__DIR__)."/vendor/autoload.php";
+            $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+            $dotenv->load();
             
+            return $_ENV;
         }
     }
