b2f37ff4178adfc51d10ba78f0fb9b34a8a0dcb4
johnwaithira <waithirajon@gmail.com>
2022-05-16 15:06:22 +0300

    Kicks Nairobi

--- database/Database.php
+++ database/Database.php
@@ -10,7 +10,7 @@
         public function __construct()
         {
             
-            $dsn = "mysql:host=127.0.0.1; dbname=n";
+            $dsn = "mysql:host=localhost; dbname=n";
             $user = "root";
             $password = "waithira";
     
