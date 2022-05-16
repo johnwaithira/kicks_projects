b59b50a81d5c5c9e1e0d05f7e91e6208e53a1695
johnwaithira <waithirajon@gmail.com>
2022-05-16 15:03:06 +0300

    Kicks Nairobi

--- database/Database.php
+++ database/Database.php
@@ -2,6 +2,8 @@
     
     namespace Waithira\Kicksfootware\database;
  
+    use PDO;
+
     class Database
     {
         public $pdo  = null;
@@ -14,11 +16,11 @@
     
             try
             {
-                $pdo = new PDO($dsn, $user, $password);
-                $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
-                $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-            }catch(Exception $e)
+                $pdo = new \PDO($dsn, $user, $password);
+                $pdo->setAttribute(\PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+                $pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES,false);
+            }catch(\Exception $e)
             {
                 echo $e->getMessage();
             }
