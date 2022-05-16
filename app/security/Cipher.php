107a4f60f137d39da1091779966ec7c77b56bccb
johnwaithira <waithirajon@gmail.com>
2022-05-16 15:56:57 +0300

    Kicks nairobi

--- app/security/Cipher.php
+++ app/security/Cipher.php
@@ -10,16 +10,36 @@
     $env = Dotenv::createImmutable(dirname($dir));
     $env->load();
 
-    var_dump($_ENV);
+
+    define("ciphering", $_ENV['CIPHERING']);
+    define("encryption_key", $_ENV['CIPHER_KEY']);
+    define("encryption_iv", $_ENV['CIPHER_IV']);
+    define("option", $_ENV['CIPHER_OPTION']);
+    
     class Cipher
     {
         public static function Encrypt($data)
         {
-        
+            return openssl_decrypt($data,
+                ciphering,
+                encryption_iv,
+                option,
+                encryption_key
+           );
         }
     
         public static function Decrypt($data)
         {
+            return openssl_decrypt($data,
+                ciphering,
+                encryption_iv,
+                option,
+                encryption_key
+            );
         
         }
     }
+
+
+    $app = new Cipher();
+    echo $app::Encrypt(123456789);
