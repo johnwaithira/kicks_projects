1fd555b696e9bbd4781b7fce2652598c7cb75817
johnwaithira <waithirajon@gmail.com>
2022-05-16 18:23:58 +0300

    Kicks nairobi

--- app/Format/Str/Elipsis.php
+++ app/Format/Str/Elipsis.php
@@ -2,7 +2,16 @@
     
     namespace Waithira\Kicksfootware\app\Format\Str;
     
-    class Elipsis
+    class Ellipsis
     {
-        
+        public static function Ellipsis($data, $len)
+        {
+           if(strlen($data) > $len){
+              $cut = substr($data, 0, $len);
+              $end = strpos($cut, '');
+              $data = $end ? substr($cut, 0, $end) : (substr($cut, $end));
+              $data .= " ...";
+            }
+            return $data;
+        }
     }
