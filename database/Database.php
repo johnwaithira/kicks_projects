48cb79fa67e880c2c31d3fd495b04566df777a2e
johnwaithira <waithirajon@gmail.com>
2022-05-16 15:02:25 +0300

    Kicks Nairobi

--- database/Database.php
+++ database/Database.php
@@ -2,38 +2,28 @@
     
     namespace Waithira\Kicksfootware\database;
  
-    use Dotenv\Dotenv;
-    use PDO;
-
-    require_once  dirname(__DIR__)."/vendor/autoload.php";
-    $dotenv = Dotenv::createImmutable(dirname(__DIR__));
-    $dotenv->load();
-
-    define("TYPE", $_ENV['DB_TYPE']);
-    define("HOST", $_ENV['DB_HOST']);
-    define("NAME", $_ENV['DB_NAME']);
-    define("USER", $_ENV['DB_USER']);
-    define("PWD", $_ENV['DB_PWD']);
     class Database
     {
-        
-        public $pdo = null;
-        public $dns  = TYPE . ":host=". HOST . ";dbname=" . NAME;
-        public $user  = USER;
-        public $pwd = PWD;
-        
+        public $pdo  = null;
         public function __construct()
         {
-            try {
-                $this->pdo = new \PDO($this->dns, $this->user, $this->pwd);
-                $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
-                echo  "connected";
-            }
-            catch (\Exception $exception)
+            
+            $dsn = "mysql:host=127.0.0.1; dbname=kicksnairobi";
+            $user = "root";
+// $password = "qnmtj420mkrk";
+            $password = "waithira";
+    
+            try
             {
-                echo $exception->getMessage();
+                $pdo = new PDO($dsn, $user, $password);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+                $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+                $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+            }catch(Exception $e)
+            {
+                echo $e->getMessage();
             }
+    
         }
     }
     
