65734370959164c173ecde710d62971fa782fdf3
johnwaithira <waithirajon@gmail.com>
2022-05-16 14:42:24 +0300

    Kicks Nairobi

--- database/Database.php
+++ database/Database.php
@@ -7,11 +7,13 @@
     class Database
     {
         public $pdo = null;
-        
+        public $dns =
         public function __construct()
         {
             try {
-            
+                $this->pdo = new \PDO($dns, $user, $pwd);
+                $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $this->pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
             }
             catch (\Exception $exception)
             {
