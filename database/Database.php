3a56d2c7998dc01cbdf5350012f186e8d693d5f0
johnwaithira <waithirajon@gmail.com>
2022-05-16 15:03:16 +0300

    Kicks Nairobi

--- database/Database.php
+++ database/Database.php
@@ -17,8 +17,8 @@
             try
             {
                 $pdo = new \PDO($dsn, $user, $password);
-                $pdo->setAttribute(\PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-                $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+                $pdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+                $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE,\PDO::FETCH_ASSOC);
                 $pdo->setAttribute(\PDO::ATTR_EMULATE_PREPARES,false);
             }catch(\Exception $e)
             {
