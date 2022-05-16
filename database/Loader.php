d2f33f5459e8740feaad1a9b4372ab2a8661b65f
johnwaithira <waithirajon@gmail.com>
2022-05-16 14:09:53 +0300

    Kicks Nairobi

--- database/Loader.php
+++ database/Loader.php
@@ -6,12 +6,20 @@
 
     class Loader
     {
-        public static function host()
+        public static function type()
         {
             require_once  dirname(__DIR__)."/vendor/autoload.php";
             $dotenv = Dotenv::createImmutable(dirname(__DIR__));
             $dotenv->load();
             
+            return $_ENV['DB_TYPE'];
+        }
+        public static function host()
+        {
+            require_once  dirname(__DIR__)."/vendor/autoload.php";
+            $dotenv = Dotenv::createImmutable(dirname(__DIR__));
+            $dotenv->load();
+        
             return $_ENV['DB_HOST'];
         }
     }
