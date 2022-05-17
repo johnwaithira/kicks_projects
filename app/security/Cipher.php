636f6e1b2d4ab0f533a283f5e133cbdd73c0662a
johnwaithira <waithirajon@gmail.com>
2022-05-17 11:19:59 +0300

    Kicks nairobi

--- app/security/Cipher.php
+++ app/security/Cipher.php
@@ -1,8 +1,33 @@
 <?php
     
     namespace Waithira\Kicksfootware\app\security;
-    define();
+    define("ciphering", "AES-128-CTR");
+    define("encryption_key", "70ee78752530ca350001ba2218726d5b1b4855d5db1d00d614322ac0921ed63af96e02ee340ecc46");
+    define("encryption_iv", "1234567890123456");
+    define("option", 0);
     class Cipher
     {
-        
+        public function Encrypt($data)
+        {
+            return openssl_encrypt($data,
+                ciphering,
+                encryption_iv,
+                option,
+                encryption_key
+            );
+        }
+    
+        public function Decrypt($data)
+        {
+            return openssl_decrypt($data,
+                ciphering,
+                encryption_iv,
+                option,
+                encryption_key
+            );
+        }
+    
     }
+    
+    $app = new Cipher();
+    echo  $app->Encrypt(434566);
