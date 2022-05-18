cd484d3dbe040dafdb7e3b12d2b86ecbbf9feb9f
johnwaithira <waithirajon@gmail.com>
2022-05-18 18:23:50 +0300

    Kicks

--- themes/client/footer.php
+++ themes/client/footer.php
@@ -4,14 +4,14 @@
         <div class="w-p-90 m-a">
             <div class="display-flex">
                 <div class="col-6">
-                    <div class="p-20">
+                    <div class="p-20-10">
                         <div class="box-shadow">
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis possimus, maxime voluptatem ipsa corrupti beatae facilis rerum reprehenderit necessitatibus cupiditate placeat labore dolores pariatur suscipit ex explicabo quasi facere sit!</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-6">
-                    <div class="p-20">
+                    <div class="p-20-10">
                         <div class="box-shadow">
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis possimus, maxime voluptatem ipsa corrupti beatae facilis rerum reprehenderit necessitatibus cupiditate placeat labore dolores pariatur suscipit ex explicabo quasi facere sit!</p>
 
