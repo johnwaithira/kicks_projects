ab28493208a6ed8833d7cffaa4cc51250338a0b8
johnwaithira <waithirajon@gmail.com>
2022-05-18 11:11:10 +0300

    Kicks

---
+++ themes/client/footer.php
@@ -0,0 +1,114 @@
+<footer>
+    <div class="footer-wrapper bg_update p-b-40 p-t-30">
+        <hr class="m-t-20 w-p-90  m-a m-b-29">
+        <div class="footer-inner-wrapper ">
+            <div class="footer-content">
+                <div class="upper-footer display-flex">
+                    <div class="col-3 col-m-5 col-s-6">
+                        <div class="footer-box">
+                            <div class="inner-box">
+                                <div class="footer-title">
+                                    <img class="w-150" src="<?php base();?>kicks_files/pngs/shoe_footer.png" alt="">
+                                </div>
+                                <div class="footer-logo-desc">
+                                    <p class="color_fade f-s-16">
+                                        Company guarante secured transaction by signing a 
+                                        debt guarantee guarantee contract with Bankfor the amount 
+                                        of cash payment by the customer
+                                    </p>
+                                </div>
+                                <div class="location">
+                                    <div class="p-t-20">
+                                        <p class="color_fade f-s-16">17 Princess Road, London, Greater London NW1 8JR, UK</p>
+                                    </div>
+                                </div>
+                                <div class="view_on_map">
+                                    <div class="m-t-35">
+                                        <button class="bg_red b-n p-17-32 b-r-4 f-s-13 c-white">View On Map</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2 col-m-3 col-s-5">
+                        <div class="footer-box">
+                            <div class="inner-box">
+                                <div class="footer-title">
+                                    <h2  class="p-l-10 f-s-18 c_black_1">Our Stores</h2>
+                                </div>
+                                <ul class="ul color_fade f-s-15">
+                                    <li><a href="">Nairobi</a></li>
+                                    <li><a href="">Thika</a></li>
+                                    <li><a href="">Ruiru</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-2 col-m-3 col-s-5">
+                        <div class="footer-box">
+                            <div class="inner-box">
+                                <div class=" footer-title">
+                                    <h2  class="p-l-10 f-s-18">Quick Links</h2>
+                                </div>
+                                <ul class="ul color_fade f-s-15">
+                                    <li><a href="">Support Center</a></li>
+                                    <li><a href="">Terms & Conditions</a></li>
+                                    <li><a href="">Shipping</a></li>
+                                    <li><a href="">Privacy Policy</a></li>
+                                    <li><a href="">Help</a></li>
+                                    <li><a href="">Products Return</a></li>
+                                    <li><a href="">FAQs</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>   
+                    <div class="col-4 p-t-10 box col-m-6 ">                
+                        <div class="footer-box">
+                            <div class="p-1-20">
+                                <div class="title">
+                                    <h2 class="f-s-18">Subscribe</h2>
+                                </div>
+                                <div class="footer-logo-desc">
+                                    <div class="p-t-10">
+                                    <p class="f-s-16">
+                                        Don’t miss to subscribe to our new feeds, 
+                                        kindly fill the form below.Company guarante secured 
+                                        transaction by signing a debt guarantee guarantee contract
+                                    </p>
+                                    </div>
+                                </div>
+                            </div>   
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="lower-footer-div w-p-100 bg_update_1">
+        <div class="lower-footer">
+            <div class="lower-inner-wrapper  p-20-0 m-t-20">
+                <div class="col-6 ">
+                    <p class="color_fade f-s-14">
+                        &copy; &nbsp; 
+                        <span id="currentYear"></span>&nbsp; 
+                        Copyrights By <?php echo $_ENV['APP_NAME'];?> - Kenya
+                    </p>
+                </div>
+                <div class="col-6">
+                    <p class="p-l-30 f-s-16">
+                        Made with ❤️ by 
+                        <a href="" class="f-w-800" style="color: rgb(0, 133,221) !important;">
+                            jonWaithira
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+    <script src="<?php base();?>ke/js/jQuery.js"></script>
+    <script src="<?php base();?>ke/js/index.js"></script>
+    <script src="<?php base();?>ke/js/app.js"></script>
+    <script src="<?php base();?>ke/js/livesearch.js"></script>
+    </body>
+</html>
