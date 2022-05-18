4af59cc48d7574c997993aa2bb0bb045f9341814
johnwaithira <waithirajon@gmail.com>
2022-05-18 19:17:03 +0300

    Kicks

--- themes/client/footer.php
+++ themes/client/footer.php
@@ -25,8 +25,8 @@
                                 <div class="view_on_map">
                                     <div class="m-t-35">
                                         <button class="bg_red b-n p-17-32 b-r-4 f-s-13 c-white"  style=" display:flex;justify-content: center;">
-                                        <img src="http://127.0.0.1/shoetailor/kicks_files/svgs/location.svg" style=" display:flex;justify-content: center;" class="w-20 p-l-10">    
-                                        <p style=" display:flex;justify-content: center;">View On Map</p>
+                                            <img src="http://127.0.0.1/shoetailor/kicks_files/svgs/location.svg" class="w-18">    
+                                            <p style=" display:flex;justify-content: center;">View On Map</p>
                                         </button>
                                     </div>
                                 </div>
