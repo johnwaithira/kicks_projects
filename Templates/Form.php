ce732b2aca1925d399b47f8f4b6e4e1250f29d3c
johnwaithira <waithirajon@gmail.com>
2022-05-18 18:07:46 +0300

    Kicks

--- Templates/Form.php
+++ Templates/Form.php
@@ -4,5 +4,27 @@
     
     class Form
     {
-        
+        public static function begin($action, $method, $params = []): Form
+        {
+            echo sprintf('<form  action="%s" method = "%s" class=" p-10 m-t-20" %s enctype="multipart/form-data">',
+            $action, $method, $params);
+            return new Form();
+        }
+        public static function end()
+        {
+            echo "
+            <script>
+                if(window.history.replaceState)
+                {
+                    window.history.replaceState(null, null, window.location.href)
+                }
+            
+            </script>
+            </form>";
+        }
+    
+        public static function field($attribute)
+        {
+            return new Fields($attribute);
+        }
     }
