1adaede2b90cec32bb71093b47e0e348cd115963
johnwaithira <waithirajon@gmail.com>
2022-05-18 18:15:54 +0300

    Kicks

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
 <?php
 
 include_once __DIR__."/themes/client/head.php";
+include_once __DIR__."/themes/client/navigation.php";
 include_once __DIR__."/themes/client/contact.php";
 include_once __DIR__."/themes/client/footer.php";
